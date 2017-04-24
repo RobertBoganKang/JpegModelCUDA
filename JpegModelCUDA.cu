289ac6dd201962cf44f53b03f569ab0300db04f0
Robert Bogan Kang <bob.kang@qq.com>
2017-04-23 23:27:55 -0500

    Update JpegModelCUDA.cu

--- JpegModelCUDA.cu
+++ JpegModelCUDA.cu
@@ -14,8 +14,7 @@ int counter = 0;
 
 void errCUDA(cudaError_t err) {
     if (err != cudaSuccess) {
-        fprintf(stderr, "Error position %d (error code %s)!\n", counter,
-                cudaGetErrorString(err));
+        fprintf(stderr, "Error position %d (error code %s)!\n", counter, cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
     counter++;
@@ -120,41 +119,46 @@ void zigzagEncodeArray(int *array, int len) {
 /*encode part*/
 __device__ float SumMatrix(float *matrix, int row, int col) {
     for (int offset = 1; offset < BLOCK; offset *= 2) {
-        if ((row % (2 * offset) == 0 && col % (2 * offset) == 0) && row < BLOCK
-            && col < BLOCK) {
-            matrix[row * BLOCK + col] += (matrix[(row) * BLOCK + (col + offset)] +
-                                          matrix[(row + offset) * BLOCK + (col)] +
-                                          matrix[(row + offset) * BLOCK + (col + offset)]);
+        if ((row % (2 * offset) == 0 && col % (2 * offset) == 0) && row < BLOCK && col < BLOCK) {
+            matrix[row * BLOCK + col] += (matrix[(row) * BLOCK + (col + offset)]
+                                          + matrix[(row + offset) * BLOCK + (col)]
+                                          + matrix[(row + offset) * BLOCK + (col + offset)]);
         }
         __syncthreads();
     }
     return matrix[0];
 }
 
-__device__ void FourierDCT(int *matrix, int *omatrix, int *Qmatrix8, int idx,
-                           int idy, int row, int col, int IMGX) {
+__device__ int Edges(int *matrix, int idx, int idy, int IMGX, int IMGY) {
+    int res;
+    res = matrix[idx * IMGX + idy];
+    if (idx > IMGX - 1 && idy > IMGY - 1)
+        res = matrix[(IMGX - 1) * IMGX + (IMGY - 1)];
+    if (idx > IMGX - 1 && idy <= IMGY - 1)
+        res = matrix[(IMGX - 1) * IMGX + idy];
+    if (idx <= IMGX - 1 && idy > IMGY - 1)
+        res = matrix[idx * IMGX + (IMGY - 1)];
+    return res;
+}
+
+__device__ void FourierDCT(int *matrix, int *omatrix, int *Qmatrix8, int idx, int idy, int row,
+                           int col, int IMGX, int IMGY) {
     __shared__ float tempmtr[BLOCK * BLOCK], tempmtr2[BLOCK * BLOCK];
     __shared__ int matrixS[BLOCK * BLOCK];
     /*the matrix needs edge handling for dimensions not the multiple of 8*/
-    matrixS[row * BLOCK + col] = matrix[idx * IMGX + idy] - 128;
+    matrixS[row * BLOCK + col] = Edges(matrix, idx, idy, IMGX, IMGY) - 128;
     __syncthreads();
     for (int v = 0; v < BLOCK; v++) {
         for (int u = 0; u < BLOCK; u++) {
             tempmtr[row * BLOCK + col] = (float) (matrixS[row * BLOCK + col] * cos((2 * row + 1) * u * M_PI / 16) *
                                                   cos((2 * col + 1) * v * M_PI / 16));
             __syncthreads();
-            float alpha = (float) (
-                    (u == 0 || v == 0) ?
-                    (u == 0 && v == 0 ?
-                     (float) (1.0 / 2.0) :
-                     sqrtf((float) (1.0 / 2.0))) :
-                    1.0);
-            tempmtr2[u * BLOCK + v] = (float) (1.0 / 4.0 * alpha
-                                               * SumMatrix(tempmtr, row, col));
+            float alpha = (float) ((u == 0 || v == 0) ? (u == 0 && v == 0 ? (float) (1.0 / 2.0) : sqrtf(
+                    (float) (1.0 / 2.0))) : 1.0);
+            tempmtr2[u * BLOCK + v] = (float) (1.0 / 4.0 * alpha * SumMatrix(tempmtr, row, col));
         }
     }
-    omatrix[row * BLOCK + col] = (int) round(
-            tempmtr2[row * BLOCK + col] / Qmatrix8[row * BLOCK + col]);
+    omatrix[row * BLOCK + col] = (int) round(tempmtr2[row * BLOCK + col] / Qmatrix8[row * BLOCK + col]);
     __syncthreads();
     matrix[idx * IMGX + idy] = omatrix[row * BLOCK + col];
 }
@@ -190,12 +194,8 @@ __device__ void InverseFourierDCT(int *matrix, int *Qmatrix8, int idx, int idy, 
     __syncthreads();
     for (int y = 0; y < BLOCK; y++) {
         for (int x = 0; x < BLOCK; x++) {
-            float alpha = (float) (
-                    (row == 0 || col == 0) ?
-                    (row == 0 && col == 0 ?
-                     (float) (1.0 / 2.0) :
-                     sqrtf((float) (1.0 / 2.0))) :
-                    1.0);
+            float alpha = (float) ((row == 0 || col == 0) ? (row == 0 && col == 0 ? (float) (1.0 / 2.0) : sqrtf(
+                    (float) (1.0 / 2.0))) : 1.0);
             tempmtr[row * BLOCK + col] = (float) (alpha * matrixS[row * BLOCK + col] *
                                                   cos((2 * x + 1) * row * M_PI / 16) *
                                                   cos((2 * y + 1) * col * M_PI / 16));
@@ -204,19 +204,18 @@ __device__ void InverseFourierDCT(int *matrix, int *Qmatrix8, int idx, int idy, 
             __syncthreads();
         }
     }
-    matrix[idx * IMGX + idy] = (int) round(
-            1.0 / 4.0 * tempmtr2[row * BLOCK + col] + 127);
+    matrix[idx * IMGX + idy] = (int) round(1.0 / 4.0 * tempmtr2[row * BLOCK + col] + 127);
     __syncthreads();
 }
 
 /*GPU operations*/
-__global__ void CUDA_MAIN(int *matrix, int *oarr, int *Qmatrix8, int *idxarr, int len, int blockX, int IMGX) {
+__global__ void CUDA_MAIN(int *matrix, int *oarr, int *Qmatrix8, int *idxarr, int len, int blockX, int IMGX, int IMGY) {
     int row = threadIdx.x, col = threadIdx.y;
     int idx = blockIdx.x * blockDim.x + row, idy = blockIdx.y * blockDim.y + col;
     int arrIdx = len * (blockIdx.x * blockX + blockIdx.y);
     __shared__ int omatrix[BLOCK * BLOCK];
     /**encoder*/
-    FourierDCT(matrix, omatrix, Qmatrix8, idx, idy, row, col, IMGX);
+    FourierDCT(matrix, omatrix, Qmatrix8, idx, idy, row, col, IMGX, IMGY);
     ZigzagEncode(omatrix, oarr, idxarr, len, arrIdx, row, col);
 
     /*Real program will divide into two parts,
@@ -236,7 +235,7 @@ __global__ void CUDA_MAIN(int *matrix, int *oarr, int *Qmatrix8, int *idxarr, in
 int main() {
     int szx = 0, szy = 0;
     int **matrixTmp = NULL;
-    matrixTmp = pgmOpen((char *) "yong.pgm", szx, szy);
+    matrixTmp = pgmOpen((char *) "i.pgm", szx, szy);
     int matrix[szx][szy];
     for (int i = 0; i < szy; ++i) {
         for (int j = 0; j < szx; ++j) {
@@ -251,12 +250,6 @@ int main() {
     int *Encodearr = (int *) malloc(2 * len * sizeof(int));
     zigzagEncodeArray(Encodearr, len);
 
-    /*print zigzag array*/
-//	cout << "Zigzag index array with length: " << len << endl;
-//	for (int i = 0; i < 2 * len; ++i) {
-//		cout << Encodearr[i] << " ";
-//	}
-//	cout << endl;
     /*zigzag index encode array*/
     int *d_Encodearr = NULL;
     errCUDA(cudaMalloc((void **) &d_Encodearr, 2 * len * sizeof(int)));
@@ -278,7 +271,7 @@ int main() {
     /*get output of zigzag array*/
     int *d_outarr = NULL;
     errCUDA(cudaMalloc((void **) &d_outarr, blockX * blockY * len * sizeof(int)));
-    CUDA_MAIN<<<grid, block>>>(d_matrix, d_outarr, d_Qmatrix8, d_Encodearr, len, blockX, szx);
+    CUDA_MAIN << < grid, block >> > (d_matrix, d_outarr, d_Qmatrix8, d_Encodearr, len, blockX, szx, szy);
 
     /*test: print zigzag array index*/
     int *outarr = (int *) malloc(blockX * blockY * len * sizeof(int));
@@ -294,7 +287,7 @@ int main() {
     }
 
     /*write image file*/
-    pgmWrite((char *) "yongJPG.pgm", matrixTmp, szx, szy);
+    pgmWrite((char *) "o.pgm", matrixTmp, szx, szy);
 
     /*destroy*/
     errCUDA(cudaFree(d_Encodearr));
